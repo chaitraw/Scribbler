b62c14d3ae0be8a18892f6fadc3b4dbc37bb15a3
Chaitra S Warad <chaitra.warad@gmail.com>
2020-04-05 17:58:08 +0530

    Added action for signIn and signUp buttons

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,16 @@
 function displaySignup() {
+    document.getElementById("signInModal").style.display = 'none';
     document.getElementById("signUpModal").style.display = 'block';
 }
 
 function closeSignup() {
     document.getElementById("signUpModal").style.display = 'none';
+}
+
+function displaySignIn() {
+    document.getElementById("signInModal").style.display = 'block';
+}
+
+function closeSignIn() {
+    document.getElementById("signInModal").style.display = 'none';
 }

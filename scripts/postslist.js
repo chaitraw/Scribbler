f8fcd3aa39a9bcba857e18c2ca3d40db31d5c04e
Chaitra S Warad <chaitra.warad@gmail.com>
2020-04-05 23:45:20 +0530

    Modified confirmation dialogue for deleting post.

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -1,14 +1,10 @@
-function removeCard(card){
-	
-	var delCard = document.getElementById(card);
-	
-	delCard.parentNode.removeChild(delCard);
-	
+function deleteCard(card){
+	var deletedCard = document.getElementById(card);
+	deletedCard.parentNode.removeChild(deletedCard);
 }
 
-function openPost(author,heading,content){
+function displayPost(author, heading, content) {
 	var url = "../html/post.html?heading=" + encodeURIComponent(heading.innerText) + "&author=" +
 	 encodeURIComponent(author.innerText) + "&content=" +encodeURIComponent(content.innerText);
      window.location.href = url; 
-	
 }

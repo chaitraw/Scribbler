622ad45beb45185fe1fc27bf7032ce1caefc547f
Chaitra S Warad <chaitra.warad@gmail.com>
2020-04-05 23:32:32 +0530

    Added feature for displaying all posts list and deleting posts

---
+++ scripts/postslist.js
@@ -0,0 +1,14 @@
+function removeCard(card){
+	
+	var delCard = document.getElementById(card);
+	
+	delCard.parentNode.removeChild(delCard);
+	
+}
+
+function openPost(author,heading,content){
+	var url = "../html/post.html?heading=" + encodeURIComponent(heading.innerText) + "&author=" +
+	 encodeURIComponent(author.innerText) + "&content=" +encodeURIComponent(content.innerText);
+     window.location.href = url; 
+	
+}
